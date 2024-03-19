e727d5e55a9ee690615daf4b497d86d9b5000feb
Добрин Ивелинов Пейчев <DIPeychev19@codingburgas.bg>
2024-03-19 11:02:55 +0200

    Add DB query

---
+++ RandomNameIDK/DataBase/WebAppDB.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE WebAppDB
+USE WebAppDB
+
+create table UserForm (
+	id INT,
+	FullName VARCHAR(50),
+	email VARCHAR(50),
+	PhoneNumber VARCHAR(50),
+	Message TEXT
+);
+insert into UserForm (id, FullName, email, PhoneNumber, Message) values (1, 'Dmitri Swynfen', 'dswynfen0@cisco.com', '3145323069', 'mi in porttitor pede justo eu massa donec dapibus duis at velit eu est congue elementum in');
+insert into UserForm (id, FullName, email, PhoneNumber, Message) values (2, 'Kimberley Summerson', 'ksummerson1@hexun.com', '6266310076', 'amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue');
+insert into UserForm (id, FullName, email, PhoneNumber, Message) values (3, 'Torin Corkan', 'tcorkan2@themeforest.net', '4265174476', 'maecenas leo odio condimentum id luctus nec molestie sed justo pellentesque viverra pede ac diam cras pellentesque volutpat');
+insert into UserForm (id, FullName, email, PhoneNumber, Message) values (4, 'Valentijn Moreing', 'vmoreing3@springer.com', '9428607090', 'at turpis donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet');
+insert into UserForm (id, FullName, email, PhoneNumber, Message) values (5, 'Mill Aulsford', 'maulsford4@berkeley.edu', '7824096912', 'potenti cras in purus eu magna vulputate luctus cum sociis natoque penatibus');
+insert into UserForm (id, FullName, email, PhoneNumber, Message) values (6, 'Mickie Caulwell', 'mcaulwell5@google.co.uk', '1157657661', 'in quis justo maecenas rhoncus aliquam lacus morbi quis tortor id');
+insert into UserForm (id, FullName, email, PhoneNumber, Message) values (7, 'Leilah Kacheler', 'lkacheler6@opera.com', '1481779263', 'posuere cubilia curae mauris viverra diam vitae quam suspendisse potenti');
+insert into UserForm (id, FullName, email, PhoneNumber, Message) values (8, 'Marten Conlaund', 'mconlaund7@shop-pro.jp', '6683700004', 'dapibus duis at velit eu est congue elementum in hac');
+insert into UserForm (id, FullName, email, PhoneNumber, Message) values (9, 'Massimo Shyram', 'mshyram8@kickstarter.com', '8845603676', 'cursus urna ut tellus nulla ut erat id mauris vulputate elementum nullam varius nulla');
+insert into UserForm (id, FullName, email, PhoneNumber, Message) values (10, 'Sylvan Linger', 'slinger9@deliciousdays.com', '1172210849', 'massa volutpat convallis morbi odio odio elementum eu interdum eu');
